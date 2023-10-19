9c4c320b50a990a9335c1da8aefce208648ec9ee
Sakam <redouan.sakam@outlook.fr>
2023-10-19 12:31:12 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,24 +1,26 @@
-#include "main.h"
-
-
-/**
- * main - Entry point
- * Description: prints _putchar, followed by a new line.
- * Return: Always 0 (Success)
- * Author: IanoNjuguna
+/*
+ * File: 0-putchar.c 
+ *
  */
 
-int main(void)
-{
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
+ #include "main.h" 
 
-return (0);
-}
+ /** 
+ * main-print putchar followed by a new line
+ * 
+ *Return :always 0
+ */
+  int main(void) 
+ {
+      _putchar('_' );
+      _putchar ('p' );
+      _putchar ('u' );
+      _putchar ('t' );
+      _putchar ('c' );
+      _putchar ('h' );
+      _putchar ('a' );
+      _putchar ('r' );
+      _putchar ('\n');
+      
+       return (0) ;
+} 
